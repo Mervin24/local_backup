8a938a42c2f83c0694c7fab0d72015b0c000647c
Mervin24 <mdalmet4@gmail.com>
2021-11-11 00:57:39 +0530

    adding allowed hosts

--- local_backup/local_backup/settings.py
+++ local_backup/local_backup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.3.224']
+ALLOWED_HOSTS = ['192.168.3.224', 'localhost', '127.0.0.1']
 
 
 # Application definition
