81d7da5f3e5adc1a71063e3f77800ac261d2b427
Mervin24 <mdalmet4@gmail.com>
2021-11-12 17:24:44 +0530

    file url fix

--- local_backup/backup/managers/fileStorageManager.py
+++ local_backup/backup/managers/fileStorageManager.py
@@ -8,9 +8,7 @@ import mimetypes
 def saveFiles(files, username):
 	fss = FileSystemStorage()
 	for file in files:
-		filename = fss.save(username+"/"+file.name, file)
-		print(filename)
-		url = fss.url(filename)
+		url = username+"/"+file.name
 		print(url)
 		fileData = File()
 		fileData.url = url
@@ -19,6 +17,7 @@ def saveFiles(files, username):
 		fileData.uploaded_by = username
 		fileData.uploaded_on = datetime.datetime.now()
 		fileData.size = file.size
+		fss.save(url, file)
 		fileaccesslayer.insertFile(fileData)
 
 def getFileExtension(fname):
@@ -32,10 +31,7 @@ def getFileForId(id):
 
 def getDownloadFileForId(id):
 	file = getFileForId(id)
-	path_to_file = settings.MEDIA_ROOT+".."+file.url
-	print(file.url)
+	path_to_file = settings.MEDIA_ROOT+file.url
 	filename = path_to_file.split('/')[-1]
-	path_to_file = path_to_file.replace(' ', "\\ ")
-	print(path_to_file)
 	mime_type, _ = mimetypes.guess_type(path_to_file)
 	return open(path_to_file, 'rb'), filename, mime_type
